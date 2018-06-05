576431a6e9e054efaa81f6803417720cdc730845
wevertonsas <borbaweverton@gmail.com>
2018-06-05 18:00:10 -0300

    First commit

---
+++ src/test/java/br/una/freelancers/FreelancersApplicationTests.java
@@ -0,0 +1,16 @@
+package br.una.freelancers;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FreelancersApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
